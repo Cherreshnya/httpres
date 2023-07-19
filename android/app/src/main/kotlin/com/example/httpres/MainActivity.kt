ebebee2cd52fe4f9728968b2a67b45823d1cec20
dashta <dashta>
2023-07-19 10:30:09 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/httpres/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.httpres
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter & HTTP',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter & HTTP'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
+
+Future<http.Response> getData() async {
+  const url = 'https://about.google/static/data/locations.json';
+  return await http.get(Uri.parse(url));
+}
+
+void loadData() {
+  getData().then((response) {
+    if(response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }).catchError((error) {
+    debugPrint(error.toString());
+  });
+}
